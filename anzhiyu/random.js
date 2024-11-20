1bcead3cb2a5bd62bb75821465efc2b023de8198
Lizhic <yan20777@outlook.com>
2024-11-20 16:11:51 +0800

    Site updated: 2024-11-20 16:11:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/30/DADD/","2024/11/09/xunfei/"];function toRandomPost(){
+var posts=["2024/10/30/DADD/","2024/11/09/xunfei/","2024/10/30/office/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
